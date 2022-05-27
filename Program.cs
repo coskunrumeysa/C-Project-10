0c6bd4a9f677cd6949ba7ed390b9bd453e533277
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-05-27 10:55:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//Beni Yaratan Elbet Yolumu Gösterir .Şuara 78
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double price;
+            price = 9.99;
+            double ex1;
+            double ex2;
+            double ex3;
+            double avg;
+            ex1 = 88.50;
+            ex2 = 50;
+            ex3 = 65.30;
+            avg = (ex1 + ex2 + ex3) / 3;
+
+            Console.WriteLine("Cost of product :"+" "+price);
+            Console.WriteLine();
+            Console.WriteLine("Average of exams :"+" "+avg);
+            Console.Read();
+        }
+    }
+}
